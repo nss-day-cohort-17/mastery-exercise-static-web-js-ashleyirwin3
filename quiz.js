aa4feeb62ac2e1a905f39693a144b7e75fbc690c
Ashley Irwin <ashleykayirwin3@gmail.com>
2016-12-02 12:27:22 -0600

    added click variable, wrote if statement and function

--- quiz.js
+++ quiz.js
@@ -1,4 +1,15 @@
+
+
 var obj = {
         key1: value1,
         key2: value2,
 };
+
+var click = document.getElementById("mouseEvent");
+
+document.addEventListener('click', function (mouseEvent) {
+        if (mouseEvent.button === 'click') {
+                console.log("Show in console")
+        }
+        return mouseEvent.button
+} )
